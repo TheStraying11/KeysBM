2d785ee45e005a4390e79f1adf2adbddecea4874
TheStraying11 <jtrycroft@gmail.com>
2022-04-11 18:36:56 +0100

    Initial commit

---
+++ main.lua
@@ -0,0 +1,12 @@
+KeysBM = {
+	frame = CreateFrame("Frame"),
+	msgs = {
+		"Nah, fuck you, no keys here.",
+		"You think i wanna do keys with you? "..args[2].." Haha, no.",
+		"Sorry, my key fell down the toilet.",
+		"Maybe you should do your own key??",
+		"My key is too good for the likes of you "..args[2],
+		"Key? whats that?"
+	}
+}
+ 
